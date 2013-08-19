dc3d64a3885e825cc9f3fb518e5d28b3e17ab4b1
DBC Apprentice <apprentice@devbootcamp.com>
2013-08-19 16:01:25 -0700

    cached + stale tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,9 @@ end
 
 get '/:username' do
   @user = TwitterUser.find_by_name(params[:username])
+  if @user.tweets_stale?
+    @user.fetch_tweets!
+  end
   @tweets = @user.tweets
   erb :list
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -2,7 +2,17 @@ class TwitterUser < ActiveRecord::Base
   # Remember to create a migration!
   has_many :tweets
 
+  def tweets_stale?
+    one_tweet = Tweet.find_by_twitter_user_id(id) rescue nil
+    if one_tweet
+      (Time.now - one_tweet.created_at) >= 900
+    else
+      true
+    end
+  end
+
   def fetch_tweets!
+    Tweet.destroy_all("twitter_user_id" => id)
     Twitter.user_timeline(name).each do |tweet|
       Tweet.create({content: tweet[:text], twitter_user: self})
     end
